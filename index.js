3c769f276b7c72dc021eb80b605525d9bd004b60
Babak Jahangiri <babakjahangiri@gmail.com>
2021-09-28 20:03:50 +0100

    init the qraphQl schema

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import schema from "./schema";
+import { graphqlHTTP } from "express-graphql";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Graphql is amazing!");
+});
+
+const root = { friend: () => {
+    return {
+        "id": 68789,
+        "firstName": "Babak",
+        "lastName": "Jahan",
+        "gender": "Male",
+        "email": "babak@babak.uk"
+    }
+}};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+); 
+
+
+app.listen(5001, ()=> console.log("running on server port localhost:5001/graphql"));
+
